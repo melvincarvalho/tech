2820552ffb3ec5dc763a08b1d7dc14f8e7a65780
Melvin Carvalho <melvincarvalho@gmail.com>
2019-10-21 23:09:59 +0200

    process html

---
+++ css.mm
@@ -0,0 +1,155 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="css" FOLDED="false" ID="ID_1831615648" CREATED="1569272472516" MODIFIED="1571334100838" ICON_SIZE="20.0 pt" LINK="index.mm" STYLE="oval">
+<icon BUILTIN="bookmark"/>
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Introduction" POSITION="right" ID="ID_1226961462" CREATED="1570979963389" MODIFIED="1571333893606" COLOR="#3333ff">
+<icon BUILTIN="wizard"/>
+<font SIZE="11" BOLD="true"/>
+<edge COLOR="#0000ff"/>
+<node TEXT="Cascading Style Sheets apply styles to documents" ID="ID_1984730658" CREATED="1571333581738" MODIFIED="1571333596174"/>
+<node TEXT="CSS is a language that describes the style of an HTML document." ID="ID_798700444" CREATED="1571333901873" MODIFIED="1571333901873"/>
+<node TEXT="CSS describes how HTML elements should be displayed." ID="ID_519099534" CREATED="1571333901873" MODIFIED="1571333901873"/>
+<node TEXT="External stylesheets are stored in CSS files" ID="ID_1914318529" CREATED="1571333943233" MODIFIED="1571333943233"/>
+</node>
+<node TEXT="Tutorials" POSITION="right" ID="ID_1831053540" CREATED="1571333645055" MODIFIED="1571333649171">
+<edge COLOR="#7c0000"/>
+<node ID="ID_179854178" CREATED="1571332849397" MODIFIED="1571332849397" LINK="https://css-tricks.com/drawing-realistic-clouds-with-svg-and-css/"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://css-tricks.com/drawing-realistic-clouds-with-svg-and-css/">https://css-tricks.com/drawing-realistic-clouds-with-svg-and-css/</a> 
+  </body>
+</html>
+</richcontent>
+<node ID="ID_1109284343" CREATED="1571334214810" MODIFIED="1571334214810" LINK="https://codepen.io/yuanchuan/pen/f70a1f9435dc90197b253b26b4d69d42"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://codepen.io/yuanchuan/pen/f70a1f9435dc90197b253b26b4d69d42">https://codepen.io/yuanchuan/pen/f70a1f9435dc90197b253b26b4d69d42</a> 
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="css" ID="ID_1182541812" CREATED="1571334261254" MODIFIED="1571334262589">
+<node TEXT="box-shadow" ID="ID_493039124" CREATED="1571334292798" MODIFIED="1571334339907" LINK="https://www.w3schools.com/cssref/css3_pr_box-shadow.asp"/>
+</node>
+<node TEXT="svg" ID="ID_879707671" CREATED="1571334640768" MODIFIED="1571334641925">
+<node TEXT="feTurbulence" ID="ID_1583470975" CREATED="1571334657013" MODIFIED="1571334669787" LINK="https://oreillymedia.github.io/Using_SVG/extras/ch16-feTurbulence.html"/>
+<node TEXT="feDisplacement" ID="ID_1991506828" CREATED="1571334662390" MODIFIED="1571334666064"/>
+</node>
+</node>
+</node>
+<node TEXT="links" POSITION="left" ID="ID_808607355" CREATED="1570968728188" MODIFIED="1570968728190">
+<icon BUILTIN="attach"/>
+<edge COLOR="#00007c"/>
+<node ID="ID_63945964" CREATED="1571333633109" MODIFIED="1571333633109" LINK="https://www.w3schools.com/css/"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://www.w3schools.com/css/">https://www.w3schools.com/css/</a> 
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1090161300" CREATED="1571333860102" MODIFIED="1571333860102" LINK="https://www.w3schools.com/cssref/default.asp"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://www.w3schools.com/cssref/default.asp">https://www.w3schools.com/cssref/default.asp</a> 
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1875564442" CREATED="1571334004203" MODIFIED="1571334004203" LINK="https://css-tricks.com/"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://css-tricks.com/">https://css-tricks.com/</a> 
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</map>
---
+++ freeplane.mm
@@ -0,0 +1,253 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="freeplane" FOLDED="false" ID="ID_1831615648" CREATED="1569272472516" MODIFIED="1571338380265" ICON_SIZE="20.0 pt" LINK="index.mm" STYLE="oval">
+<icon BUILTIN="bookmark"/>
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Summary" POSITION="right" ID="ID_1226961462" CREATED="1570979963389" MODIFIED="1570980209554" COLOR="#3333ff">
+<icon BUILTIN="wizard"/>
+<font SIZE="11" BOLD="true"/>
+<edge COLOR="#0000ff"/>
+<node TEXT="Freeplane is a fork of free mind for mind mapping" ID="ID_492559976" CREATED="1571338349855" MODIFIED="1571338360932"/>
+</node>
+<node TEXT="Details" POSITION="right" ID="ID_120250381" CREATED="1570979967089" MODIFIED="1570980228711">
+<icon BUILTIN="list"/>
+<edge COLOR="#00ff00"/>
+<font BOLD="true"/>
+</node>
+<node TEXT="Doing" POSITION="right" ID="ID_1085389952" CREATED="1571344051119" MODIFIED="1571344055220">
+<edge COLOR="#00ffff"/>
+<node TEXT="xslt" ID="ID_121554319" CREATED="1571346855057" MODIFIED="1571346859158">
+<node ID="ID_1455200437" CREATED="1571346860812" MODIFIED="1571346860812" LINK="https://github.com/htw-docear/docear/blob/master/freeplane/resources/xslt/freeplane2html.xsl"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://github.com/htw-docear/docear/blob/master/freeplane/resources/xslt/freeplane2html.xsl">https://github.com/htw-docear/docear/blob/master/freeplane/resources/xslt/freeplane2html.xsl</a> 
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1309887860" CREATED="1571347033471" MODIFIED="1571347033471" LINK="https://github.com/freeplane/freeplane/blob/2fe1447b5e67e7ffaa8ad7c31eb46c83bb0c83f3/freeplane/src/editor/resources/xslt/freeplane2html.xsl"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://github.com/freeplane/freeplane/blob/2fe1447b5e67e7ffaa8ad7c31eb46c83bb0c83f3/freeplane/src/editor/resources/xslt/freeplane2html.xsl">https://github.com/freeplane/freeplane/blob/2fe1447b5e67e7ffaa8ad7c31eb46c83bb0c83f3/freeplane/src/editor/resources/xslt/freeplane2html.xsl</a> 
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="links" POSITION="left" ID="ID_808607355" CREATED="1570968728188" MODIFIED="1570968728190">
+<icon BUILTIN="attach"/>
+<edge COLOR="#00007c"/>
+<node ID="ID_1248490359" CREATED="1571338424296" MODIFIED="1571338424296" LINK="https://github.com/freeplane/freeplane"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://github.com/freeplane/freeplane">https://github.com/freeplane/freeplane</a> 
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1288808904" CREATED="1571338505640" MODIFIED="1571338505640" LINK="https://www.freeplane.org/"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://www.freeplane.org/">https://www.freeplane.org/</a> 
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1162793213" CREATED="1571344020507" MODIFIED="1571344020507" LINK="https://www.freeplane.org/wiki/"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://www.freeplane.org/wiki/">https://www.freeplane.org/wiki/</a> 
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="docs" POSITION="left" ID="ID_1100215200" CREATED="1570969039178" MODIFIED="1570980265221">
+<icon BUILTIN="folder"/>
+<edge COLOR="#ff00ff"/>
+<node TEXT="scripting" ID="ID_443931931" CREATED="1571338437687" MODIFIED="1571338442222" LINK="https://www.freeplane.org/wiki/index.php/Scripting">
+<node ID="ID_456322394" CREATED="1571344008796" MODIFIED="1571344008796" LINK="https://www.freeplane.org/wiki/index.php/Scripts_collection"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://www.freeplane.org/wiki/index.php/Scripts_collection">https://www.freeplane.org/wiki/index.php/Scripts_collection</a> 
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="build" ID="ID_1145562489" CREATED="1571344451920" MODIFIED="1571344453360">
+<node ID="ID_1428523306" CREATED="1571344454536" MODIFIED="1571344454536" LINK="https://www.freeplane.org/wiki/index.php/IDE_setup"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://www.freeplane.org/wiki/index.php/IDE_setup">https://www.freeplane.org/wiki/index.php/IDE_setup</a> 
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1942546707" CREATED="1571344466573" MODIFIED="1571344466573" LINK="https://www.freeplane.org/wiki/index.php/Category:Coding"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://www.freeplane.org/wiki/index.php/Category:Coding">https://www.freeplane.org/wiki/index.php/Category:Coding</a> 
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1260132184" CREATED="1571344474221" MODIFIED="1571344474221" LINK="https://www.freeplane.org/wiki/index.php/Category:Developer"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://www.freeplane.org/wiki/index.php/Category:Developer">https://www.freeplane.org/wiki/index.php/Category:Developer</a> 
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="addons" ID="ID_916552440" CREATED="1571344496089" MODIFIED="1571344497346">
+<node ID="ID_607657161" CREATED="1571344498683" MODIFIED="1571344498683" LINK="https://www.freeplane.org/wiki/index.php/Add-ons_(Develop)"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://www.freeplane.org/wiki/index.php/Add-ons_(Develop)">https://www.freeplane.org/wiki/index.php/Add-ons_(Develop)</a> 
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="contributors" ID="ID_846778807" CREATED="1571344526148" MODIFIED="1571344528240">
+<node ID="ID_562906381" CREATED="1571344529297" MODIFIED="1571344529297" LINK="https://www.freeplane.org/wiki/index.php/Contributors"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://www.freeplane.org/wiki/index.php/Contributors">https://www.freeplane.org/wiki/index.php/Contributors</a> 
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="forum" ID="ID_1131779541" CREATED="1571344587203" MODIFIED="1571344589490">
+<node ID="ID_4426340" CREATED="1571344601646" MODIFIED="1571344601646" LINK="https://sourceforge.net/p/freeplane/discussion/758437/"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://sourceforge.net/p/freeplane/discussion/758437/">https://sourceforge.net/p/freeplane/discussion/758437/</a> 
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="tags" POSITION="left" ID="ID_466759422" CREATED="1570968754383" MODIFIED="1571340808305">
+<icon BUILTIN="hashtag"/>
+<edge COLOR="#007c00"/>
+</node>
+</node>
+</map>
