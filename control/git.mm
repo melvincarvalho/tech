e53a7ecbd1598083662bf9fb3cbf40155ff8b116
Melvin Carvalho <melvincarvalho@gmail.com>
2019-12-24 07:15:54 +0100

    add git reset

--- control/git.mm
+++ control/git.mm
@@ -85,17 +85,25 @@
     <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://www.atlassian.com/git/tutorials/undoing-changes/git-reset">https://www.atlassian.com/git/tutorials/undoing-changes/git-reset</a> 
   </body>
 </html>
-
 </richcontent>
 </node>
 <node TEXT="soft" ID="ID_625336574" CREATED="1577143578880" MODIFIED="1577143580705">
 <node TEXT="commit tree" ID="ID_1871528466" CREATED="1577143767770" MODIFIED="1577143770339"/>
 </node>
-<node TEXT="mixed" ID="ID_1578045062" CREATED="1577143581385" MODIFIED="1577143582848">
-<node TEXT="staging index" ID="ID_1338796809" CREATED="1577143772184" MODIFIED="1577143774906"/>
+<node TEXT="mixed" ID="ID_1578045062" CREATED="1577143581385" MODIFIED="1577164554828">
+<font BOLD="true"/>
+<node TEXT="staging index" ID="ID_1338796809" CREATED="1577143772184" MODIFIED="1577143774906">
+<node TEXT="his tree is tracking Working Directory changes, that have been promoted with git add, to be stored in the next commit. This tree is a complex internal caching mechanism." ID="ID_1679719810" CREATED="1577163829512" MODIFIED="1577163829512"/>
+<node TEXT="git ls-files" ID="ID_1444858658" CREATED="1577163895174" MODIFIED="1577163899120">
+<node TEXT="-s or --stage" ID="ID_1177229065" CREATED="1577163960997" MODIFIED="1577163964858"/>
+</node>
+</node>
 </node>
 <node TEXT="hard" ID="ID_948378930" CREATED="1577143584246" MODIFIED="1577143585635">
-<node TEXT="working directory" ID="ID_286677452" CREATED="1577143777292" MODIFIED="1577143780071"/>
+<node TEXT="working directory" ID="ID_286677452" CREATED="1577143777292" MODIFIED="1577143780071">
+<node TEXT="your local tree" ID="ID_242765237" CREATED="1577162795287" MODIFIED="1577162798580"/>
+</node>
+<node TEXT="Reset the staging area and the working directory to match the most recent commit. In addition to unstaging changes, the --hard flag tells Git to overwrite all changes in the working directory, too. Put another way: this obliterates all uncommitted changes, so make sure you really want to throw away your local developments before using it." ID="ID_897335891" CREATED="1577168125680" MODIFIED="1577168125680"/>
 </node>
 </node>
 </node>
