d7c4d13e58c727108f88f67ee5f5b49aed00aef3
Melvin Carvalho <melvincarvalho@gmail.com>
2020-01-04 14:36:21 +0100

    Add cryptocurrencies

---
+++ cryptocurrencies/bitcoin/transactions.mm
@@ -0,0 +1,95 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="transactions" FOLDED="false" ID="ID_555710409" CREATED="1578144579705" MODIFIED="1578144698023" LINK="index.mm" STYLE="oval">
+<edge STYLE="bezier" COLOR="#00007c" WIDTH="thin" DASH="SOLID"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="21" RULE="ON_BRANCH_CREATION"/>
+<font SIZE="18"/>
+<node POSITION="right" ID="ID_329238139" CREATED="1578144752124" MODIFIED="1578144752132" LINK="https://github.com/ChristopherA/Learning-Bitcoin-from-the-Command-Line/blob/master/07_3_Scripting_a_P2PKH.md"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://github.com/ChristopherA/Learning-Bitcoin-from-the-Command-Line/blob/master/07_3_Scripting_a_P2PKH.md">https://github.com/ChristopherA/Learning-Bitcoin-from-the-Command-Line/blob/master/07_3_Scripting_a_P2PKH.md</a> 
+  </body>
+</html>
+
+</richcontent>
+<edge COLOR="#7c007c"/>
+</node>
+<node TEXT="example" POSITION="right" ID="ID_747891472" CREATED="1578144852858" MODIFIED="1578144854755">
+<edge COLOR="#007c7c"/>
+<node TEXT="Script: &lt;signature&gt; &lt;pubKey&gt; OP_DUP OP_HASH160 &lt;pubKeyHash&gt; OP_EQUALVERIFY OP_CHECKSIG" ID="ID_1438371809" CREATED="1578144858172" MODIFIED="1578144858172"/>
+<node TEXT="signature" ID="ID_1608239053" CREATED="1578144863259" MODIFIED="1578144867038">
+<node TEXT="pubkey" ID="ID_945071350" CREATED="1578144867889" MODIFIED="1578144870755"/>
+</node>
+<node TEXT="OP_DUP" ID="ID_1662853924" CREATED="1578144872206" MODIFIED="1578144874831"/>
+<node TEXT="HASH160" ID="ID_1271193085" CREATED="1578144879948" MODIFIED="1578144882662"/>
+<node TEXT="&lt;pubkeyHash&gt;" ID="ID_727501130" CREATED="1578144883451" MODIFIED="1578144891111"/>
+<node TEXT="OP_EQUALVERIFY" ID="ID_1005246884" CREATED="1578144892397" MODIFIED="1578144899196"/>
+<node TEXT="OP_CHECKSIG" ID="ID_656592081" CREATED="1578144900661" MODIFIED="1578144904739"/>
+</node>
+</node>
+</map>
